d2bbc94c1cf888d6b28a86c18e03486fbce3da70
Dorothy Mwangi <dmwangiw@gmail.com>
2019-05-22 11:36:58 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/positivevibesgradle2/PositiveVibesGradle2Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.positivevibesgradle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositiveVibesGradle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositiveVibesGradle2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/positivevibesgradle2/PositiveVibesGradle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.positivevibesgradle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositiveVibesGradle2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
